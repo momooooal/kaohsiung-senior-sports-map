6dd888aebb468b0a818ca27f39376ecc2549cb5d
momooooal <momooooal@gmail.com>
2026-08-13 08:49:21 +0800

    Delete admin-config.js

--- admin-config.js
+++
@@ -1,5 +0,0 @@
-// 場館／據點資料新增或修改申請入口
-// 完成 Google Apps Script 部署後，把 /exec 網址貼到 portalUrl。
-window.RESOURCE_REQUEST_CONFIG = {
-  portalUrl: "https://script.google.com/macros/s/AKfycbyHuuaw054vOenYKQqFCiXw6l2aUiBadc1r2kruqPYn9zLVtqhwTds3Em3JzlVr0b_ajg/exec"
-};
